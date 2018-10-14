9aaba454b50e3e2a35362a0994433b7b37dafe4e
Carlos Bueno <carlos.bueno@opinno.com>
2018-10-14 08:02:42 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Carlos Miguel Bueno Lujan on 14/10/18.
+//  Copyright © 2018 Carlos Miguel Bueno Lujan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
